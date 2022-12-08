d74f2b88667ff967a6d797aa4f68047b41d8c9e0
Developer <error@Error.error>
2022-12-08 23:24:04 +0600

    first commt

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vofojnw.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+app.get('/', async (req, res) => {
+    res.send('Running Tripwalletbd')
+})
+
+app.listen(port, () => console.log(`tripwallet running on ${port}`))
