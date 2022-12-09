f053fbba9831b96f7dea01b31d5431aee0447a23
Developer <error@Error.error>
2022-12-09 18:20:21 +0600

    sing

--- index.js
+++ index.js
@@ -1,19 +1,47 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const { query } = require('express');
 require('dotenv').config();
+// const jwt = require('jsonwebtoken');c
 const port = process.env.PORT || 5000;
+
 const app = express();
 
+//middleware
 app.use(cors());
 app.use(express.json());
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vofojnw.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
+
+async function run() {
+  try {
+    const tourCollection = client.db('tripwallet').collection('alltours');
+
+
+    app.get('/tours', async (req, res) => {
+      let query = {};
+      const userData = await carsCollection.find(query).toArray();
+      res.send(userData);
+    })
+
+
+
+  }
+  finally {
+
+  }
+}
+run().catch(console.log);
+
+
+
 app.get('/', async (req, res) => {
-    res.send('Running Tripwalletbd')
+  res.send('tripwallet server is running')
 })
 
 app.listen(port, () => console.log(`tripwallet running on ${port}`))
